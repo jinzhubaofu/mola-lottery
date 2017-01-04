3bc4d2fa5f724f2b94ae7dc14c3ff85e6e7744a1
jinzhubaofu <leonlu@outlook.com>
2017-01-04 11:26:17 +0800

    同步修改 example

--- example/index.js
+++ example/index.js
@@ -14,6 +14,8 @@ ReactDOM.render(
         frameImage="http://gss3.bdstatic.com/5foUcz3n1MgCo2Kml5_Y_D3/static/asset/activity/supergirl/turnTable/img/kuangs1.5c709d00.png"
         prizesImage="http://gss3.bdstatic.com/5foUcz3n1MgCo2Kml5_Y_D3/static/asset/activity/supergirl/turnTable/img/gifts.95173542.png"
         prizeAmount='8'
-        datasource="http://voice.baidu.com/api/lottery?page=27&act=9&preview=1" />,
+        datasource={{
+            endpoint: 'http://voice.baidu.com/api/lottery?page=27&act=9&preview=1'
+        }} />,
     document.getElementById('app')
 );
